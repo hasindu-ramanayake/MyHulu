51d331b1500ca63c18e625f50c219ca3f3786bd6
hasindu-ramanayake <you@example.com>
2022-08-01 00:10:51 +0530

    myhulu
    project setup complete

---
+++ hulu-clone/pages/index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <title>MyHulu</title>
+      <h1>Hello Next.js</h1>
+      
+    </div>
+  )
+}
---
+++ hulu-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode:"jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,html}",
+    "./components/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
